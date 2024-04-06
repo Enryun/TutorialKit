196399b880d51f8a3aa10ed50422de593a79ba30
JamesThang <jamesthang1996@gmail.com>
2024-04-06 13:09:42 +0700

    Initial Commit

---
+++ Sources/TutorialKit.xcframework/ios-arm64_x86_64-simulator/TutorialKit.framework/Headers/TutorialKit.h
@@ -0,0 +1,18 @@
+//
+//  TutorialKit.h
+//  TutorialKit
+//
+//  Created by James Thang on 31/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TutorialKit.
+FOUNDATION_EXPORT double TutorialKitVersionNumber;
+
+//! Project version string for TutorialKit.
+FOUNDATION_EXPORT const unsigned char TutorialKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TutorialKit/PublicHeader.h>
+
+
